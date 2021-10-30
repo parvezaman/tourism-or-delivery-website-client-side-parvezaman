38131beb62c89841536e2b27c7bdf0f5c9782230
parvezaman <emailtoamanullah@gmail.com>
2021-10-30 22:53:44 +0600

    error handled: no empty field in add service

--- src/pages/AddService/AddService.js
+++ src/pages/AddService/AddService.js
@@ -7,14 +7,15 @@ const AddService = () => {
     const { register, handleSubmit, reset } = useForm();
     const onSubmit = data => {
         console.log(data);
-
+    
         axios.post('http://localhost:5000/services', data)
         .then(res => {
+            console.log(res);
+
             if(res.data.insertedId){
                 alert("Thank You!!! Service Added Successfully!");
                 reset();
             }
-            console.log(res);
         })
         };
     return (
@@ -22,10 +23,10 @@ const AddService = () => {
             <h3 className="text-center">Let's Add A Service</h3>
 
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("name")} placeholder="Add a Name to the Service/Package" />
-                <textarea {...register("description")} placeholder="Would you mind adding a little description" />
-                <input type="number" {...register("price")} placeholder="Suggest us your desiered price level" />
-                <input {...register("imageUrl")} placeholder="Give us related image url" />
+                <input {...register("name")} placeholder="Add a Name to the Service/Package" required />
+                <textarea {...register("description")} placeholder="Would you mind adding a little description" required/>
+                <input type="number" {...register("price")} placeholder="Suggest us your desiered price level" required/>
+                <input {...register("imageUrl")} placeholder="Give us related image url" required/>
                 <input className="btn btn-info" type="submit" />
             </form>
         </div>
