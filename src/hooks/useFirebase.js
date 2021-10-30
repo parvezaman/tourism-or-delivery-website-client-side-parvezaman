65e0372fcb3a373f66a371a535ffe5aebcab8a18
parvezaman <emailtoamanullah@gmail.com>
2021-10-30 15:34:49 +0600

    ridirect to booking after login

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -14,7 +14,7 @@ const useFirebase = ()=>{
     const signInUsingGoogle=()=>{
         setIsLoading(true);
         const googleProvider = new GoogleAuthProvider();
-        signInWithPopup(auth, googleProvider)
+        return signInWithPopup(auth, googleProvider)
         .then(result =>{
             setUser(result.user)
         })
--- src/pages/Shared/Login/Login.js
+++ src/pages/Shared/Login/Login.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth'
 
 const Login = () => {
     const {signInUsingGoogle} = useAuth();
+    const location = useLocation();
+    const history = useHistory();
+
+    const redirect_uri = location.state?.from || '/home';
+
+    const handleGoogleLogin =()=>{
+        signInUsingGoogle()
+        .then(result =>{
+            history.push(redirect_uri);
+        })
+        // .finally(()=> setIsLoading(false));
+    }
 
 
     return (
         <div className="text-center mt-5">
             <h3>Lets login</h3>
             <br /><br />
-            <button onClick={signInUsingGoogle} className="btn btn-warning">Log in with Google</button>
+            <button onClick={handleGoogleLogin} className="btn btn-warning">Log in with Google</button>
         </div>
     );
 };
