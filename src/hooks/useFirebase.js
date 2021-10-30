ecffa1eb8f5b5020fba078db5d16d9eb3c4f6a6e
parvezaman <emailtoamanullah@gmail.com>
2021-10-30 13:40:59 +0600

    reload and redirect to login page issue solved with added spinner

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -7,15 +7,18 @@ initializeAuthentication();
 
 const useFirebase = ()=>{
     const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     const auth = getAuth();
 
     const signInUsingGoogle=()=>{
+        setIsLoading(true);
         const googleProvider = new GoogleAuthProvider();
         signInWithPopup(auth, googleProvider)
         .then(result =>{
             setUser(result.user)
         })
+        .finally(()=> setIsLoading(false));
     }
 
     // tracking user state change
@@ -27,17 +30,21 @@ const useFirebase = ()=>{
             else{
                 setUser({});
             }
+            setIsLoading(false);
         })
         return ()=> unsubscribed;
     },[])
 
     const logOut =()=>{
+        setIsLoading(true);
         signOut(auth)
         .then(()=>{})
+        .finally(()=> setIsLoading(false));
     }
 
     return{
         user,
+        isLoading,
         signInUsingGoogle,
         logOut
     }
