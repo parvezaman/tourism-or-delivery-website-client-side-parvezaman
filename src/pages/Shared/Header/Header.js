4280a5bf3f64c0045b14d8d4e724d58b0434a6a9
parvezaman <emailtoamanullah@gmail.com>
2021-10-30 12:16:55 +0600

    signed in as made dynamic

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -16,11 +16,11 @@ const Header = () => {
                         <Nav.Link as={Link} to="/home#services">Services</Nav.Link>
                         <Nav.Link as={Link} to="/home#ourguides">Our Guides</Nav.Link>
                         {user?.email ?
-                            <Button onClick={logOut}  variant="secondary">Logout</Button> :
+                            <Button onClick={logOut}  variant="danger">Logout</Button> :
                             <Nav.Link as={Link} to="/login">Login</Nav.Link>}
 
-                        <Navbar.Text>
-                            Signed in as: <a href="#login">{user?.displayName}</a>
+                        <Navbar.Text className="ms-2">   
+                           { user.email && `Signed in as:` }<a className="ms-2" href="#login">{user?.displayName}</a>
                         </Navbar.Text>
                     </Navbar.Collapse>
                 </Container>
